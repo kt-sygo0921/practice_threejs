1deb8b51e01b46d58fdd36c76e177ed8c8377081
unknown <>
2016-11-01 23:57:15 +0900

    ディレクトリ名修正/ラジアン

--- practice/ts/chapter-02/07-both-cameras.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../../../typings/index.d.ts" />
-
-function init() {
-    
-}
-window.onload = init;
--- practice/ts/chapter-02/practice/test.ts
+++ practice/ts/chapter-02/practice/test.ts
@@ -14,16 +14,32 @@ function init() {
     renderer.setSize(width, height);
     renderer.setClearColor(0xffffff,1.0);
 
+    //plane作成
+    var planegeom = new THREE.PlaneGeometry(180,180);
+    var planemate = new THREE.MeshLambertMaterial({color:0xff0000});
+    var plane = new THREE.Mesh(planegeom,planemate);
+    plane.rotation.x = -(90 * Math.PI) / 180;
+    // plane.rotation.y = 0.5;
+    scene.add(plane);
+
     //カメラの作成
     var camera = new THREE.PerspectiveCamera(45, width/height,0.1,1000);
-    camera.position.set(200,200,200);
+    camera.position.set(500,500,500);
+    camera.lookAt(scene.position);
 
     //ライトの作成
-    var light = new THREE.AmbientLight();
+    var light = new THREE.AmbientLight(0x292929);
     scene.add(light);
 
-
+    //マテリアル追加
+    var cubegeom = new THREE.BoxGeometry(5,5,5);
+    var cubemate = new THREE.MeshLambertMaterial({color:0x000000});
+    var cube = new THREE.Mesh(cubegeom,cubemate);
+    cube.position.set(1,1,1);
+    scene.add(cube);
     
-
-    
+    renderer.render(scene,camera);
+    document.getElementById("WebGL-output").appendChild(renderer.domElement);
 }
+
+window.onload = init;
